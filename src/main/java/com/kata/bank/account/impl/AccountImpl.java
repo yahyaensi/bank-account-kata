9c6ada0157a40cf29bdfa6bd126468e4244eb467
Yahya <yahya.romdhane.ensi@gmail.com>
2019-04-03 22:06:58 +0200

    Create a basic structure implementation for account interface

---
+++ src/main/java/com/kata/bank/account/impl/AccountImpl.java
@@ -0,0 +1,54 @@
+package com.kata.bank.account.impl;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kata.bank.account.Account;
+
+/**
+ * Implementation of customer account.
+ * 
+ * @author Yahya
+ * @version 1.0.0
+ */
+public class AccountImpl implements Account {
+	
+	/**
+	 * Account's balance.
+	 */
+	private BigDecimal balance = BigDecimal.ZERO;
+	
+	private List<Operation> operationList = new ArrayList<>();
+
+	@Override
+	public void depositMoney(BigDecimal amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BigDecimal withdrawMoney(BigDecimal amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigDecimal getBalance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasSufficientBalance(BigDecimal amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void showHistory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
