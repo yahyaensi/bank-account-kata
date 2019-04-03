7f1b709d61080650d8bf6244fb9d0c2a5d83dbe2
Yahya <yahya.romdhane.ensi@gmail.com>
2019-04-03 22:05:19 +0200

    Create account contract interface

---
+++ src/main/java/com/kata/bank/account/Account.java
@@ -0,0 +1,49 @@
+package com.kata.bank.account;
+
+import java.math.BigDecimal;
+
+/**
+ * Customer account contract.
+ * 
+ * @author Yahya
+ * @version 1.0.0
+ */
+public interface Account {
+
+	/**
+	 * Adds money to the account.
+	 * 
+	 * @param amount
+	 *            the money to add
+	 */
+	public void depositMoney(BigDecimal amount);
+
+	/**
+	 * Withdraws money from the account.
+	 * 
+	 * @param amount
+	 *            the money to withdraw
+	 */
+	public BigDecimal withdrawMoney(BigDecimal amount);
+
+	/**
+	 * Gets the account balance.
+	 * 
+	 * @return the account's balance
+	 */
+	public BigDecimal getBalance();
+	
+    /**
+     * Checks if the account has a sufficient balance
+     * 
+	 * @param amount
+	 *            the money to withdraw
+     */
+    boolean hasSufficientBalance(BigDecimal amount);
+    
+    /**
+     * 
+     */
+    void showHistory();
+
+}
